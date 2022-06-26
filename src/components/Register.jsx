a193711db0cff10d038d51b3ad6e6bb9202c432d
Sofia.Guido <sofia.guido@ucrso.info>
2022-06-26 10:11:47 -0600

    commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from 'axios';
+const serverURL = "https://localhost:44304/api/Users";
 function Register() {
+  
+
+const [data, setData] = useState([]);
+const[userSelection, setUserSelection]=useState({
+     name: '',
+     lastName: '',
+     phone: '',
+     email: '',
+     password: ''
+})
+
+const handleChange=e=>{
+  const {name, value}= e.target;
+  setUserSelection({
+    ...userSelection,
+    [name]: value
+  });
+  console.log(userSelection);
+}
+
+const peticionPost=async()=>{
+  alert('uwu');
+  await axios.post(serverURL, userSelection)
+  .then(response=>{
+    setData(data.concat(response.data));
+  }).catch(error=> {
+    console.log(error);
+    alert(error);
+  })
+}
+
   return (
     <div className="App bg-gray-900 h-screen w-screen relative overflow-hidden flex justify-center items-center">
       <div className="h-40-r w-40-r bg-gradient-to-r from-orange-400 to-amber-300 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
       <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
 
       <div className="container px-4 h-96 w-110 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
-        <form className="h-full flex flex-col justify-evenly items-center">
+        <form className="h-full flex flex-col justify-evenly items-center" onSubmit={()=>peticionPost()}>
           <div className="text-white font-poppins text-2xl tracking-widest">
             Registro de Usuario
           </div>
@@ -17,6 +49,8 @@ function Register() {
               type="text"
               placeholder="nombre"
               className="input-text float-right mx-3"
+              name="name"
+              onChange={handleChange}
             />
           </div>
           <div className="flow-root">
@@ -25,14 +59,8 @@ function Register() {
               type="text"
               placeholder="apellido"
               className="input-text float-right mx-3"
-            />
-          </div>
-          <div className="flow-root">
-            <h2 className="text-white float-left">Nombre de usuario</h2>
-            <input
-              type="text"
-              placeholder="nombre de usuario"
-              className="input-text float-right mx-3"
+              name="lastName"
+              onChange={handleChange}
             />
           </div>
           <div className="flow-root">
@@ -41,6 +69,8 @@ function Register() {
               type="text"
               placeholder="número de teléfono"
               className="input-text float-right mx-3"
+              name="phone"
+              onChange={handleChange}
             />
           </div>
           <div className="flow-root">
@@ -49,6 +79,8 @@ function Register() {
               type="text"
               placeholder="correo electrónico"
               className="input-text float-right mx-3"
+              name="email"
+              onChange={handleChange}
             />
           </div>
           <div className="flow-root">
@@ -57,6 +89,8 @@ function Register() {
               type="password"
               placeholder="contraseña"
               className="input-text float-right mx-3"
+              name="password"
+              onChange={handleChange}
             />
           </div>
           <div className="flow-root">
@@ -65,6 +99,8 @@ function Register() {
               type="password"
               placeholder="contraseña"
               className="input-text float-right mx-3"
+              onChange={handleChange}
+
             />
           </div>
 
@@ -72,11 +108,13 @@ function Register() {
             type="Submit"
             value="Registrarse"
             className="cursor-pointer font-poppins rounded-full px-6 py-1 bg-white bg-opacity-50 hover:bg-white hover:bg-opacity-80 "
-          />
+           />
         </form>
       </div>
     </div>
   );
+
+ 
 }
 
 export default Register;
